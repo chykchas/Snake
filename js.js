8478ed587ff9953cc2a46199242c625f4db15025
Nik <chykchas.mail@gmail.com>
2021-09-02 16:43:13 +0300

    add key events

--- js.js
+++ js.js
@@ -1,1 +1,17 @@
 let field = document.getElementById("field");
+
+document.addEventListener("keydown", function (elem) {
+  if (elem.keyCode == 37) {
+    dy = 0;
+    dx = -move;
+  } else if (elem.keyCode == 38) {
+    dy = -move;
+    dx = 0;
+  } else if (elem.keyCode == 39) {
+    dy = 0;
+    dx = move;
+  } else if (elem.keyCode == 40) {
+    dy = move;
+    dx = 0;
+  }
+});
